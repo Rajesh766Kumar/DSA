8c5683313b4cb251e1fa84a11b999d42eb16de64
Rajesh Kumar <rpy41137@gmail.com>
2025-06-26 13:50:05 +0530

    commit

---
+++ genrateFindestl.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+#include<string>
+
+int main (){
+    string a(5, 'j');
+    cout<<a<<"\n";
+    string j=
+}
---
+++ stl.cpp
@@ -0,0 +1,16 @@
+// standred template libray
+
+
+#include<iostream>
+#include<string> 
+using namespace std;
+
+int main() {
+    string a;
+    cout << "Enter name:\n";
+    getline(cin, a);  
+    cout << "Your name = " << a << endl; 
+    return 0;
+}
+
+
